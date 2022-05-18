fc9e990cbab69768ecb7c0d06dd2dc54e96ed9bd
JohnKondo <john13011@gmail.com>
2022-05-18 14:34:53 +0200

    Leaflet is back
    next step is to remove it again to put an image instead

---
+++ api/map.js
@@ -0,0 +1,62 @@
+import {Dimensions} from "react-native";
+
+let screenHeight = Dimensions.get("window").height;
+export const map = String.raw `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="" />
+        <style type="text/css">
+            #map{ /* la carte DOIT avoir une hauteur sinon elle n'apparaît pas */
+                bottom: 0;
+                left: 0;
+                right: 0;
+                top: 0;
+                position: fixed;
+            }
+        </style>
+        <title>Carte</title>
+    </head>
+    <body>
+        <div id="map">
+        <!-- Ici s'affichera la carte -->
+    </div>
+
+        <!-- Fichiers Javascript -->
+        <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+    <script type="text/javascript">
+            // On initialise la latitude et la longitude de Paris (centre de la carte)
+            var lat = 43.23205;
+            var lon = 5.43915;
+            var macarte = null;
+            // Fonction d'initialisation de la carte
+            var villes = {
+                "Paris": { "lat": 43.23205, "lon": 5.439 },
+                "Brest": { "lat": 43.23205, "lon": 5.43915 },
+                "Quimper": { "lat": 43.23205, "lon": 5.43 },
+                "Bayonne": { "lat": 43.23205, "lon": 5.43916 }
+            };
+            // Fonction d'initialisation de la carte
+            function initMap() {
+            // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+                macarte = L.map('map').setView([lat, lon], 15);
+            // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+                L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+            // Il est toujours bien de laisser le lien vers la source des données
+                attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
+                minZoom: 1,
+                maxZoom: 20
+            }).addTo(macarte);
+    // Nous parcourons la liste des villes
+            for (ville in villes) {
+                var marker = L.marker([villes[ville].lat, villes[ville].lon]).addTo(macarte);
+            }                   
+            }
+            window.onload = function(){
+        // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+        initMap(); 
+            };
+        </script>
+    </body>
+</html>`
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
-import {View, StyleSheet, Dimensions} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import {View, StyleSheet, Dimensions, Image} from 'react-native';
 import * as Location from 'expo-location'
+import {TouchableOpacity} from "react-native-gesture-handler";
+import { map } from "../api/map";
+import WebView from "react-native-webview";
+
 
 
 
 
 const MapScreen = ({ navigation }) => {
   const [location, setLocation] = useState({});
-  const [mapRegion, setmapRegion] = useState({
+    const [mapRef, setMapRef] = useState(null);
+    const [mapRegion, setmapRegion] = useState({
+
     latitude: 43.23205,
     longitude: 5.43915,
     latitudeDelta: 0.01335,
     longitudeDelta: 0.00795,
   });
   return (
-      <View>
-        <MapView style={{ alignSelf: 'stretch', height: '100%' }} region={mapRegion}>
-          <Marker coordinate={mapRegion} title='Marker' />
-        </MapView>
+      <View style={styles.mainArea}>
+
+          <WebView
+              ref={(webViewRef) => {
+                  setMapRef(webViewRef);
+              }}
+              source={{ html: map }}
+              style={styles.mapStyle}
+
+          />
+
+          <View style={styles.logoArea}>
+              <TouchableOpacity>
+                  <Image
+                      source={require("../assets/images/logo.png")}
+                      style={styles.reportLogo}
+                  />
+              </TouchableOpacity>
+          </View>
+
       </View>
   );
 };
@@ -30,5 +51,24 @@ const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-
+    mainArea: {
+        flex: 1,
+        justifyContent: "flex-end",
+    },
+    logoArea: {
+        position: "absolute",
+        alignSelf: "flex-end",
+        bottom: 30,
+        right: 30,
+    },
+    reportLogo: {
+        height: 70,
+        width: 70,
+    },
+    mapStyle: {
+        height: screenHeight,
+        width: screenWidth,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+    },
 });
