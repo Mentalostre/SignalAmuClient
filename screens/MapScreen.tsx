dc8348242b7efe35854802642f51e385f0cae0dc
JohnKondo <john13011@gmail.com>
2022-06-08 22:03:29 +0200

    Revert "Report modal update"
    This reverts commit 0a5daa3c932d1b67c673aaf0eae7b52af8831ed2.

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -63,7 +63,7 @@ const MapScreen = ({navigation}) => {
     const checkNotEmptyField = ()=>{
         return (reportDesc == null || reportLevel == null);
     }
-
+    
     const handleReport =async () => {
         if(checkNotEmptyField()) {
             alert("Remplissez tous les champs")
@@ -192,6 +192,10 @@ const MapScreen = ({navigation}) => {
                     >
                         <KeyboardAvoidingView behavior="padding">
                         <View style={styles.reportModalView}>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    toggleReportModal()
+                                }}>
                                 <TextInput
                                     style={styles.textInputStyle}
                                     placeholderTextColor="#3983cd"
@@ -200,8 +204,9 @@ const MapScreen = ({navigation}) => {
                                     multiline={true}
                                     placeholder="Description"
                                     onChangeText={(reportDesc) => setReportDesc(reportDesc)}
-                                    maxLength={255}
                                 />
+
+                            </TouchableOpacity>
                             <View style={styles.reportSlideView}>
                             <Slider
                                 minimumValue={1}
